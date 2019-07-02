5270dd3a0dcc05dc65036c14f8236a0ebd316f27
ferbosh <boshfernando@gmail.com>
2019-07-01 20:37:38 -0500

    First commit

---
+++ bar/__init__.py
@@ -0,0 +1,11 @@
+class Drink:
+    def __init__(self):
+        super(Drink, self).__init__()
+        pass
+    pass
+
+
+class Consumer:
+    def __init__(self):
+        super(Consumer, self).__init__()
+    pass
